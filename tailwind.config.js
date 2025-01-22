7d37e02d6e2bb6f48a766f4c0d267a9faf3768de
gokul <rgokul689@gmail.com>
2025-01-22 23:37:24 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        blues : {
+          100: '#050022',
+          200: '#31a8de'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
